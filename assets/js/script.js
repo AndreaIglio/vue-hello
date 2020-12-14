1d3d09dea66a5852752291dc18b96d70410cf043
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-14 16:00:14 +0100

    Task BONUS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,12 @@
 // TASK 1 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 let app = new Vue({
-
-
-
-    el :"#title",
-    data: {
-        message : 'Ciao Ajeje',
-    }
-
-})
+  el: "#title",
+  data: {
+    message: `Ciao Ajeje it's ` + new Date().toLocaleString(),
+    image: "http://qnimate.com/wp-content/uploads/2014/03/images2.jpg",
+  },
+});
 
 
 
