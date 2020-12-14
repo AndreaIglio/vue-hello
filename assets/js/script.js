1ef687484f994497a3d623340fdd004251551a04
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-14 15:31:25 +0100

    Task 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 // Istruzioni:
-// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// TASK 1 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+let app = new Vue({
+
+
+
+    el :"#title",
+    data: {
+        message : 'Ciao Ajeje',
+    }
+
+})
+
+
+
 // Bonus: Aggiungere un’immagine presa anch’essa da un data
